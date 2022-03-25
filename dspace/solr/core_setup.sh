42cb2f2f111f583ca4c04a7102360bd31f6afbce
Steve Brush <steve.brush@apption.com>
2022-03-24 21:47:19 -0400

    update scripts, dockerfile

--- dspace/solr/core_setup.sh
+++ dspace/solr/core_setup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
-    
-     entrypoint:
-    - /bin/bash
-    - '-c'
-    - |
-      init-var-solr
-      precreate-core authority /opt/solr/server/solr/configsets/authority
-      cp -r -u /opt/solr/server/solr/configsets/authority/* authority
-      precreate-core oai /opt/solr/server/solr/configsets/oai
-      cp -r -u /opt/solr/server/solr/configsets/oai/* oai
-      precreate-core search /opt/solr/server/solr/configsets/search
-      cp -r -u /opt/solr/server/solr/configsets/search/* search
-      precreate-core statistics /opt/solr/server/solr/configsets/statistics
-      cp -r -u /opt/solr/server/solr/configsets/statistics/* statistics
-      exec solr -f
+init-var-solr
+precreate-core authority /opt/solr/server/solr/configsets/authority
+cp -r -u /opt/solr/server/solr/configsets/authority/* authority
+precreate-core oai /opt/solr/server/solr/configsets/oai
+cp -r -u /opt/solr/server/solr/configsets/oai/* oai
+precreate-core search /opt/solr/server/solr/configsets/search
+cp -r -u /opt/solr/server/solr/configsets/search/* search
+precreate-core statistics /opt/solr/server/solr/configsets/statistics
+cp -r -u /opt/solr/server/solr/configsets/statistics/* statistics
+exec solr -f
