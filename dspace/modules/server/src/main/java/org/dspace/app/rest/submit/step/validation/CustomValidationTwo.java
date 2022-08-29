9b9948133934b88aba13fa8e91fde0493d4a0103
jjcc <jjccforth@gmail.com>
2022-08-28 21:48:22 -0400

    fixed the error caused by merge

--- dspace/modules/server/src/main/java/org/dspace/app/rest/submit/step/validation/CustomValidationTwo.java
+++ dspace/modules/server/src/main/java/org/dspace/app/rest/submit/step/validation/CustomValidationTwo.java
@@ -46,6 +46,8 @@ public class CustomValidationTwo extends AbstractValidation {
 
     private MetadataAuthorityService metadataAuthorityService;
 
+    private List<ErrorRest> errors = new ArrayList<ErrorRest>();
+
     @Override
     public List<ErrorRest> validate(SubmissionService submissionService, InProgressSubmission obj,
                                     SubmissionStepConfig config) throws DCInputsReaderException, SQLException {
@@ -97,4 +99,43 @@ public class CustomValidationTwo extends AbstractValidation {
         this.inputReader = inputReader;
     }
 
+
+
+    //Following are copy from the old class of AbstractValidation before 7.3. To keep the merged code working temprary
+    /**
+     * Add an error message (i18nKey) for a specific json path
+     *
+     * @param i18nKey
+     *            the validation error message as a key to internationalize
+     * @param path
+     *            the json path that identify the wrong data in the submission. It could be as specific as a single
+     *            value in a multivalued attribute or general of a "whole" section
+     */
+    public void addError(String i18nKey, String path) {
+        boolean found = false;
+        if (StringUtils.isNotBlank(i18nKey)) {
+            for (ErrorRest error : errors) {
+                if (i18nKey.equals(error.getMessage())) {
+                    error.getPaths().add(path);
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) {
+            ErrorRest error = new ErrorRest();
+            error.setMessage(i18nKey);
+            error.getPaths().add(path);
+            errors.add(error);
+        }
+    }
+
+    /**
+     * Expose the identified errors
+     *
+     * @return the list of identified {@link ErrorRest}
+     */
+    public List<ErrorRest> getErrors() {
+        return errors;
+    }
 }
