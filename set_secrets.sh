4bcfd64f36955b4c5f8950c6d42f004c1d86b06f
steve.brush@apption.com <steve.brush@apption.com>
2023-02-22 11:09:28 -0500

    Pipeline updates

--- set_secrets.sh
+++ set_secrets.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 echo setting DB password
-export DB_PASSWORD=$(cat ./secret.txt)
+export DB_PASSWORD=$(cat ./db_password.txt)
 sed -i $(printf 's/@db-password@/%s/g' "$DB_PASSWORD") /app/dspace/config/local.cfg
+
+echo setting storage account
+export STORAGE_ACCOUNT=$(cat ./storage_account.txt)
+sed -i $(printf 's/@storage-account@/%s/g' "$STORAGE_ACCOUNT") /app/dspace/config/local.cfg
