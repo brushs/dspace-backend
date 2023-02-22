5bd4bdb1aee90500fd743d3f8cf9fd219acd50b2
steve.brush@apption.com <steve.brush@apption.com>
2023-02-22 11:49:36 -0500

    Update set_secrets.sh

--- set_secrets.sh
+++ set_secrets.sh
@@ -5,4 +5,4 @@ sed -i $(printf 's/@db-password@/%s/g' "$DB_PASSWORD") /app/dspace/config/local.
 
 echo setting storage account
 export STORAGE_ACCOUNT=$(cat ./storage_account.txt)
-sed -i $(printf 's/@storage-account@/%s/g' "$STORAGE_ACCOUNT") /app/dspace/config/local.cfg
+sed -i $(printf 's:@storage-account@:%s:g' "$STORAGE_ACCOUNT") /app/dspace/config/local.cfg
