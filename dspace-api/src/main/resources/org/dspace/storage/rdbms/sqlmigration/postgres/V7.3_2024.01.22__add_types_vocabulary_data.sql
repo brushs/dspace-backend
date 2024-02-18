cfc1adbd1eb98528afaba756e1ebe2c79ade5d73
steve.brush@apption.com <steve.brush@apption.com>
2024-02-18 14:20:53 -0500

    Fixed typo

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.3_2024.01.22__add_types_vocabulary_data.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.3_2024.01.22__add_types_vocabulary_data.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO vocabulary VALUES (6, 'Publication Types', '*Publication Types', '', '', true, 'system', current_timestamp, 'system', current_timestamp);
 
-INSERT INTO term VALUES (60001, null,'Abstract ','Résumé', '', '', true, 6, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (60001, null,'Abstract','Résumé', '', '', true, 6, null, 'system', current_timestamp, 'system', current_timestamp);
 INSERT INTO term VALUES (60002, null,'Article','Article de périodique', '', '', true, 6, null, 'system', current_timestamp, 'system', current_timestamp);
 INSERT INTO term VALUES (60003, null,'Book','Livre', '', '', true, 6, null, 'system', current_timestamp, 'system', current_timestamp);
 INSERT INTO term VALUES (60004, null,'Book Chapter','Chapitre de livre', '', '', true, 6, null, 'system', current_timestamp, 'system', current_timestamp);
