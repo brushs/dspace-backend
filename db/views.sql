f943894aa29abd26e64b0fe2cf51b08c701c342b
steve.brush@apption.com <steve.brush@apption.com>
2024-04-19 15:12:23 -0400

    Update person view

--- db/views.sql
+++ db/views.sql
@@ -28,12 +28,16 @@ from
 CREATE OR REPLACE VIEW person_summary_v AS
 select
     a.dspace_object_id,
-    c.text_value as entity_type,
     b.text_value as first_name,
     e.text_value as last_name,
-    f.text_value as dpsid,
+    m.text_value as orcid,
     g.text_value as migration_id,
-    d.text_value as date_added
+    f.text_value as dpsid,
+    h.text_value as nrnuserid,
+    i.text_value as cfsid,
+    d.text_value as date_added,
+    coalesce(author_count,0) as author_count,
+    coalesce(mono_author_count,0) as mono_author_count
 from
     (select dspace_object_id
      from metadatavalue
@@ -47,11 +51,6 @@ from
         join
     (select text_value, dspace_object_id
      from metadatavalue
-     where metadata_field_id = 7) c
-    on a.dspace_object_id = c.dspace_object_id
-        join
-    (select text_value, dspace_object_id
-     from metadatavalue
      where metadata_field_id = 18) d
     on a.dspace_object_id = d.dspace_object_id
         left outer join
@@ -62,13 +61,40 @@ from
         left outer join
     (select text_value, dspace_object_id
      from metadatavalue
-     where metadata_field_id = 281) f
+     where metadata_field_id = 465) f
     on a.dspace_object_id = f.dspace_object_id
         left outer join
     (select text_value, dspace_object_id
      from metadatavalue
      where metadata_field_id = 217) g
-    on a.dspace_object_id = g.dspace_object_id;
+    on a.dspace_object_id = g.dspace_object_id
+        left outer join
+    (select text_value, dspace_object_id
+     from metadatavalue
+     where metadata_field_id = 466) h
+    on a.dspace_object_id = h.dspace_object_id
+        left outer join
+    (select text_value, dspace_object_id
+     from metadatavalue
+     where metadata_field_id = 457) i
+    on a.dspace_object_id = i.dspace_object_id
+        left outer join
+    (select text_value, dspace_object_id
+     from metadatavalue
+     where metadata_field_id = 365) m
+    on a.dspace_object_id = m.dspace_object_id
+        left outer join
+    (select count(*) as author_count, right_id
+     from relationship
+     where type_id = 1
+     group by right_id) j
+    on a.dspace_object_id = j.right_id
+        left outer join
+    (select count(*) as mono_author_count, right_id
+     from relationship
+     where type_id = 2
+     group by right_id) k
+    on a.dspace_object_id = k.right_id;
 
 CREATE OR REPLACE VIEW metadata_language_summary_v AS
 select
