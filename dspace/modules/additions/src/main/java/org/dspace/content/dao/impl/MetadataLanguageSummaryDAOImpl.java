34eb48ab1a32464c45d1a73dc02e4d5ed049ad1f
steve.brush@apption.com <steve.brush@apption.com>
2024-04-25 14:46:19 -0400

    Switch to stored proc

--- dspace/modules/additions/src/main/java/org/dspace/content/dao/impl/MetadataLanguageSummaryDAOImpl.java
+++ dspace/modules/additions/src/main/java/org/dspace/content/dao/impl/MetadataLanguageSummaryDAOImpl.java
@@ -16,7 +16,9 @@ import org.dspace.core.Context;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.jdbc.Work;
+import org.hibernate.procedure.ProcedureCall;
 
+import javax.persistence.ParameterMode;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -49,6 +51,16 @@ public class MetadataLanguageSummaryDAOImpl extends AbstractHibernateDAO<Metadat
     public void refreshMaterializedView(Context context) {
         try (Session session = getHibernateSession(context)) {
 
+            // Create a ProcedureCall for the stored procedure
+            ProcedureCall procedureCall = session.createStoredProcedureCall("refresh_materialized_view");
+
+            // Bind the parameter value
+            //procedureCall.registerParameter("mv_name", String.class, ParameterMode.IN).bindValue("metadata_language_summary_mv");
+
+            // Execute the stored procedure
+            procedureCall.execute();
+
+            /*
             session.doWork(new Work() {
                 @Override
                 public void execute(java.sql.Connection connection) throws java.sql.SQLException {
@@ -61,6 +73,7 @@ public class MetadataLanguageSummaryDAOImpl extends AbstractHibernateDAO<Metadat
                     }
                 }
             });
+            */
 
             log.info("Committed");
         } catch (Exception e) {
