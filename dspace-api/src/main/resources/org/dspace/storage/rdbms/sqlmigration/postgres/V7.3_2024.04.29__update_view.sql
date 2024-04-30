0fcf440db989384ce47c9067b9cb82323870c4f7
steve.brush@apption.com <steve.brush@apption.com>
2024-04-29 22:17:28 -0400

    Fix view to exclude items not in archive

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.3_2024.04.29__update_view.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.3_2024.04.29__update_view.sql
@@ -1,101 +1,4 @@
-CREATE OR REPLACE VIEW item_summary_v AS
-select
-    a.dspace_object_id,
-    c.text_value as entity_type,
-    b.text_value as title,
-    d.text_value as date_added
-from
-    (select dspace_object_id
-     from metadatavalue
-     group by dspace_object_id
-    ) a
-        join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 73 and text_lang = 'en') b
-    on a.dspace_object_id = b.dspace_object_id
-        join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 7) c
-    on a.dspace_object_id = c.dspace_object_id
-        join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 18) d
-    on a.dspace_object_id = d.dspace_object_id;
-
-CREATE OR REPLACE VIEW person_summary_v AS
-select
-    a.dspace_object_id,
-    b.text_value as first_name,
-    e.text_value as last_name,
-    m.text_value as orcid,
-    g.text_value as migration_id,
-    f.text_value as dpsid,
-    h.text_value as nrnuserid,
-    i.text_value as cfsid,
-    d.text_value as date_added,
-    coalesce(author_count,0) as author_count,
-    coalesce(mono_author_count,0) as mono_author_count
-from
-    (select dspace_object_id
-     from metadatavalue
-     group by dspace_object_id
-    ) a
-        join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 353) b
-    on a.dspace_object_id = b.dspace_object_id
-        join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 18) d
-    on a.dspace_object_id = d.dspace_object_id
-        left outer join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 354) e
-    on a.dspace_object_id = e.dspace_object_id
-        left outer join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 465) f
-    on a.dspace_object_id = f.dspace_object_id
-        left outer join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 217) g
-    on a.dspace_object_id = g.dspace_object_id
-        left outer join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 466) h
-    on a.dspace_object_id = h.dspace_object_id
-        left outer join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 457) i
-    on a.dspace_object_id = i.dspace_object_id
-        left outer join
-    (select text_value, dspace_object_id
-     from metadatavalue
-     where metadata_field_id = 365) m
-    on a.dspace_object_id = m.dspace_object_id
-        left outer join
-    (select count(*) as author_count, right_id
-     from relationship
-     where type_id = 1
-     group by right_id) j
-    on a.dspace_object_id = j.right_id
-        left outer join
-    (select count(*) as mono_author_count, right_id
-     from relationship
-     where type_id = 2
-     group by right_id) k
-    on a.dspace_object_id = k.right_id;
-
+DROP MATERIALIZED VIEW metadata_language_summary_mv;
 CREATE MATERIALIZED VIEW metadata_language_summary_mv AS
 select
     i.uuid as item_id,
@@ -187,5 +90,7 @@ from
      group by dspace_object_id) subject_fr
     on typ.dspace_object_id = subject_fr.dspace_object_id
     where i.owning_collection is not null;
+
+GRANT ALL ON metadata_language_summary_mv to dspace;
 
 CREATE UNIQUE INDEX idx_mdls_item_id ON metadata_language_summary_mv (item_id);
