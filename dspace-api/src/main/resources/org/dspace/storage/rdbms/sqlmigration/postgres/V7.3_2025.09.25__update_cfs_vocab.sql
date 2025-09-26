79479ec704498c7ac4197fde0c1bf2e37df493f0
Steve Brush <steve.brush@apption.com>
2025-09-25 20:32:17 -0400

    Update CFS vocabs

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.3_2025.09.25__update_cfs_vocab.sql
@@ -0,0 +1,8 @@
+INSERT INTO term VALUES (51400, null,'Indigenous fire stewardship','Intendance autochtone du feu', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51401, null,'Prescribed fire','Brûlage dirigé', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+
+DELETE FROM term WHERE id = 50195;
+
+INSERT INTO term VALUES (30070, null,'Wildland fires','Feux de végétation', '', '', true, 3, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (30071, null,'Ecology','Écologie', '', '', true, 3, null, 'system', current_timestamp, 'system', current_timestamp);
+
