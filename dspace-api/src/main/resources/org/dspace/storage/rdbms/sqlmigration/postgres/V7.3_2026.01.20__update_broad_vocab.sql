6d057ad5a139bf54fff1ecc30533648db7131a5f
Steve Brush <steve.brush@apption.com>
2026-01-20 14:13:02 -0500

    Add new term to Broad vocab

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.3_2026.01.20__update_broad_vocab.sql
@@ -0,0 +1,2 @@
+INSERT INTO term VALUES (30072, null,'National Tree Seed Centre','Centre national de semences forestières', '', '', true, 3, null, 'system', current_timestamp, 'system', current_timestamp);
+
