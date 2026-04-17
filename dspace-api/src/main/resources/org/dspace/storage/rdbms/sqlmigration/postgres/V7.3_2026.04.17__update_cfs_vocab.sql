d4811efc3cc6b396300559c7296d6747af05773e
Steve Brush <steve.brush@apption.com>
2026-04-17 14:19:29 -0400

    Added new terms to CFS vocabulary

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.3_2026.04.17__update_cfs_vocab.sql
@@ -0,0 +1,9 @@
+INSERT INTO term VALUES (51402, null,'Acadian','Acadienne', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51403, null,'Boreal','Boréale', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51404, null,'Carolinian','Carolinienne', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51405, null,'Coastal','Côtière', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51406, null,'Columbia','Columbia', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51407, null,'Great Lakes–St Lawrence','Grands Lacs et du Saint-Laurent', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51408, null,'Montane','Montagnarde', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+INSERT INTO term VALUES (51409, null,'Subalpine','Subalpine', '', '', true, 5, null, 'system', current_timestamp, 'system', current_timestamp);
+
