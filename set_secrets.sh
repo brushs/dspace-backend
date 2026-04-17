4c85c0766c7e2db6eef583908d2ecf24de71ba65
Steve Brush <steve.brush@apption.com>
2026-04-17 13:52:32 -0400

    Revert "Tweaks to allow this to be released"
    This reverts commit 2e363356b5e36dd471c0844e961669169d833f4c.

--- set_secrets.sh
+++ set_secrets.sh
@@ -15,6 +15,6 @@ echo setting crossref email
 export STORAGE_ACCOUNT=$(cat ./crossref_email.txt)
 sed -i $(printf 's:@crossref-email@:%s:g' "$STORAGE_ACCOUNT") /app/dspace/config/local.cfg
 
-#echo setting gc notify key
-#export STORAGE_ACCOUNT=$(cat ./gc_notify_key.txt)
-#sed -i $(printf 's:@gc-notify-key@:%s:g' "$STORAGE_ACCOUNT") /app/dspace/config/local.cfg
+echo setting gc notify key
+export STORAGE_ACCOUNT=$(cat ./gc_notify_key.txt)
+sed -i $(printf 's:@gc-notify-key@:%s:g' "$STORAGE_ACCOUNT") /app/dspace/config/local.cfg
